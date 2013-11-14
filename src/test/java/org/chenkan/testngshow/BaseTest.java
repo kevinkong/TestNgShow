bf462964f242d52b1f4087849d682b439e08d231
CHEN Kan <ckbsc@126.com>
2013-11-14 14:33:40 +0800

    [misc] 优化Log

--- src/test/java/org/chenkan/testngshow/BaseTest.java
+++ src/test/java/org/chenkan/testngshow/BaseTest.java
@@ -10,49 +10,49 @@ public class BaseTest {
     @BeforeSuite
     public void doBeforeSuiteA() {
         HelloWorld helloWorld = new HelloWorld();
-        helloWorld.echo("-- Before Suite A --");
+        helloWorld.echo("-- BaseTest.java -- Before Suite A --");
     }
 
     @BeforeSuite
     public void doBeforeSuiteB() {
         HelloWorld helloWorld = new HelloWorld();
-        helloWorld.echo("-- Before Suite B --");
+        helloWorld.echo("-- BaseTest.java -- Before Suite B --");
     }
 
     @AfterSuite
     public void doAfterSuiteA() {
         HelloWorld helloWorld = new HelloWorld();
-        helloWorld.echo("-- After Suite A --");
+        helloWorld.echo("-- BaseTest.java -- After Suite A --");
     }
 
     @AfterSuite
     public void doAfterSuiteB() {
         HelloWorld helloWorld = new HelloWorld();
-        helloWorld.echo("-- After Suite B --");
+        helloWorld.echo("-- BaseTest.java -- After Suite B --");
     }
 
     @BeforeTest
     public void doBeforeTestA() {
         HelloWorld helloWorld = new HelloWorld();
-        helloWorld.echo("-- Before Test A --");
+        helloWorld.echo("-- BaseTest.java -- Before Test A --");
     }
 
     @BeforeTest
     public void doBeforeTestB() {
         HelloWorld helloWorld = new HelloWorld();
-        helloWorld.echo("-- Before Test B --");
+        helloWorld.echo("-- BaseTest.java -- Before Test B --");
     }
 
     @AfterTest
     public void doAfterTestA() {
         HelloWorld helloWorld = new HelloWorld();
-        helloWorld.echo("-- After Test A --");
+        helloWorld.echo("-- BaseTest.java -- After Test A --");
     }
 
     @AfterTest
     public void doAfterTestB() {
         HelloWorld helloWorld = new HelloWorld();
-        helloWorld.echo("-- After Test B --");
+        helloWorld.echo("-- BaseTest.java -- After Test B --");
     }
 
 }
