e5d262fb0499c03563d28f0d34d7e90811e924b5
CHEN Kan <ckbsc@126.com>
2013-11-14 11:54:51 +0800

    [dev] 加入被测试class - HelloWorld.java

---
+++ src/main/java/org/chenkan/testngshow/HelloWorld.java
@@ -0,0 +1,15 @@
+package org.chenkan.testngshow;
+
+public class HelloWorld {
+
+    /**
+     * Echo function
+     * @param input
+     * @return
+     */
+    public String echo(String input) {
+        System.out.println("-- ECHO -- " + input);
+        return input;
+    }
+
+}
