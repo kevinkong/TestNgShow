8858e79384f0e3b636a3dd0be91a7e53f82aa73b
CHEN Kan <ckbsc@126.com>
2013-11-14 13:59:34 +0800

    [dev] 第一个demo，显示各个annotation的执行顺序 - 设置xml文件

--- src/test/java/org/chenkan/testngshow/TestBanana.java
+++ src/test/java/org/chenkan/testngshow/TestBanana.java
@@ -2,53 +2,53 @@ package org.chenkan.testngshow;
 
 import org.testng.annotations.*;
 
-public class TestApple extends BaseTest {
+public class TestBanana extends BaseTest {
 
     @BeforeClass
     public void doBeforeClassA() {
         HelloWorld helloWorld = new HelloWorld();
-        helloWorld.echo("-- TestApple.java -- Before Class A --");
+        helloWorld.echo("-- TestBanana.java -- Before Class A --");
     }
 
     @BeforeClass
     public void doBeforeClassB() {
         HelloWorld helloWorld = new HelloWorld();
-        helloWorld.echo("-- TestApple.java -- Before Class B --");
+        helloWorld.echo("-- TestBanana.java -- Before Class B --");
     }
 
     @BeforeMethod
     public void doBeforeMethodA() {
         HelloWorld helloWorld = new HelloWorld();
-        helloWorld.echo("-- TestApple.java -- Before Method A --");
+        helloWorld.echo("-- TestBanana.java -- Before Method A --");
     }
 
     @BeforeMethod
     public void doBeforeMethodB() {
         HelloWorld helloWorld = new HelloWorld();
-        helloWorld.echo("-- TestApple.java -- Before Method B --");
+        helloWorld.echo("-- TestBanana.java -- Before Method B --");
     }
 
     @Test
     public void doTestA() {
         HelloWorld helloWorld = new HelloWorld();
-        helloWorld.echo("-- TestApple.java -- Test A --");
+        helloWorld.echo("-- TestBanana.java -- Test A --");
     }
 
     @Test
     public void doTestB() {
         HelloWorld helloWorld = new HelloWorld();
-        helloWorld.echo("-- TestApple.java -- Test B --");
+        helloWorld.echo("-- TestBanana.java -- Test B --");
     }
 
     @AfterMethod
     public void doAfterMethod() {
         HelloWorld helloWorld = new HelloWorld();
-        helloWorld.echo("-- TestApple.java -- After Method --");
+        helloWorld.echo("-- TestBanana.java -- After Method --");
     }
 
     @AfterClass
     public void doAfterClass() {
         HelloWorld helloWorld = new HelloWorld();
-        helloWorld.echo("-- TestApple.java -- After Class --");
+        helloWorld.echo("-- TestBanana.java -- After Class --");
     }
 }
